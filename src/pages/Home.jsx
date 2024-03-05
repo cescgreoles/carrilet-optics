9fd9a1caab388e4e06db10faf84001222c4ce533
Cesc Greoles <antoniogreoles@Mac-Cesc-Greoles.local>
2024-03-05 17:10:25 +0100

    modify cookies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "../styles/Home.scss";
 import NavigationInformation from "../components/NavigationInformation";
 import NavigationBar from "../components/NavigationBar";
@@ -7,8 +8,22 @@ import Imagen from "../components/Imagen";
 import Combination from "../components/Combination";
 import NestedModal from "../components/SeccionModal";
 import BannerPublicitario from "../components/BannerPublicitario";
+import CookieBanner from "../components/CookieBanner";
 
 const Home = () => {
+  const [showCookieBanner, setShowCookieBanner] = useState(true);
+
+  useEffect(() => {
+    // Verificar si el usuario ya ha aceptado las cookies
+    const hasAcceptedCookies = localStorage.getItem("cookiesAccepted");
+    setShowCookieBanner(!hasAcceptedCookies);
+  }, []);
+
+  const handleAcceptCookies = () => {
+    // Guardar en el almacenamiento local que el usuario ha aceptado las cookies
+    localStorage.setItem("cookiesAccepted", "true");
+    setShowCookieBanner(false);
+  };
   return (
     <div className="container">
       <NavigationInformation />
@@ -18,6 +33,7 @@ const Home = () => {
       <Combination />
       <Footer />
       <BannerPublicitario />
+      {showCookieBanner && <CookieBanner onAccept={handleAcceptCookies} />}
     </div>
   );
 };
