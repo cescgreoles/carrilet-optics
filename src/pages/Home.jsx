83c320052f4ed4edb1f5c507645295e858feb200
Cesc Greoles <antoniogreoles@Mac-Cesc-Greoles.local>
2024-03-26 19:03:58 +0100

    cookies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,8 @@ import Imagen from "../components/Imagen";
 import Combination from "../components/Combination";
 import NestedModal from "../components/SeccionModal";
 import BannerPublicitario from "../components/BannerPublicitario";
-import CookieBanner from "../components/CookieBanner";
 
 const Home = () => {
-  const [showCookieBanner, setShowCookieBanner] = useState(true);
-
-  useEffect(() => {
-    // Verificar si el usuario ya ha aceptado las cookies
-    const hasAcceptedCookies = localStorage.getItem("cookiesAccepted");
-    setShowCookieBanner(!hasAcceptedCookies);
-  }, []);
-
-  const handleAcceptCookies = () => {
-    // Guardar en el almacenamiento local que el usuario ha aceptado las cookies
-    localStorage.setItem("cookiesAccepted", "true");
-    setShowCookieBanner(false);
-  };
   return (
     <div className="container">
       <NavigationInformation />
@@ -33,7 +19,6 @@ const Home = () => {
       <Combination />
       <Footer />
       <BannerPublicitario />
-      {showCookieBanner && <CookieBanner onAccept={handleAcceptCookies} />}
     </div>
   );
 };
